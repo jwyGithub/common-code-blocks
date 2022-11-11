76b7f1e086b61092b8a251939b02fad28eec8f6e
江伟业 <58636312+jwyGithub@users.noreply.github.com>
2022-11-11 08:05:44 +0000

    init

---
+++ code/node/index.ts
@@ -0,0 +1,72 @@
+import fs from 'fs';
+import path from 'path';
+
+/**
+ * @description 是否是文件夹
+ * @param path
+ * @returns { Boolean }
+ */
+function isDirectory(path: string): Boolean {
+    return fs.statSync(path).isDirectory();
+}
+
+/**
+ * @description 是否是文件
+ * @param path
+ * @returns { Boolean }
+ */
+function isFile(path: string): Boolean {
+    return fs.statSync(path).isFile();
+}
+
+/**
+ * @description 获取所有文件夹
+ * @param cwd
+ * @returns { Array<{fullpath: string;dirname: string;}> }
+ */
+function getDirs(cwd: string = process.cwd()): Array<{ fullpath: string; dirname: string }> {
+    const dirs = fs.readdirSync(cwd);
+    return dirs.reduce((result, dir) => {
+        if (isDirectory(path.join(cwd, dir))) {
+            result.push({
+                fullpath: path.join(cwd, dir),
+                dirname: dir
+            });
+        }
+        return result;
+    }, [] as Array<{ fullpath: string; dirname: string }>);
+}
+
+/**
+ * @description 获取所有文件
+ * @param cwd
+ * @returns { Array<{fullpath: string;filename: string;}> }
+ */
+function getFiles(cwd: string = process.cwd()): Array<{ fullpath: string; filename: string }> {
+    const dirs = fs.readdirSync(cwd);
+    return dirs.reduce((result, dir) => {
+        if (isFile(path.join(cwd, dir))) {
+            result.push({
+                fullpath: path.join(cwd, dir),
+                filename: dir
+            });
+        }
+        return result;
+    }, [] as Array<{ fullpath: string; filename: string }>);
+}
+
+function getDirsAndFiles(cwd: string = process.cwd()):{
+    files: { fullpath: string; filename: string }[],
+    dirs:{ fullpath: string; dirname: string }
+} {
+    const dirs = fs.readdirSync(cwd);
+    return dirs.reduce((result, dir) => {
+        if (isFile(path.join(cwd, dir))) {
+            result.
+        }
+        return result;
+    }, {});
+}
+
+console.log(getFiles());
+
